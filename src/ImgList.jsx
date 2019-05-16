8abb78c61281437eea8d76017ee47a3da46fdc01
Nimm <yandreyev@yahoo.com>
2019-05-16 09:02:49 -0500

    icons

--- src/ImgList.jsx
+++ src/ImgList.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import classnames from "classnames";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Done from "@material-ui/icons/Done";
+import Create from "@material-ui/icons/Create";
 import Drawer from "@material-ui/core/Drawer";
 import Loading from "./Loading";
 import StatesView from "./StatesView";
@@ -12,6 +15,36 @@ const ImgList = ({ classes, state, db, states, setNav }) => {
   const openDrawer = () => {
     setOpen(true);
   };
+  const renderIconsContainer = img => {
+    if (img.marked || img.drawing || img.drawn) {
+      return (
+        <div
+          className={classnames(
+            classes.iconsContainer,
+            img.drawing ? classes.drawingIconsContainer : null
+          )}
+        >
+          {img.marked ? (
+            <Done
+              className={classnames(
+                classes.icon,
+                img.drawing ? classes.drawingIcon : null
+              )}
+            />
+          ) : null}
+          {img.done ? (
+            <Create
+              className={classnames(
+                classes.icon,
+                img.drawing ? classes.drawingIcon : null
+              )}
+            />
+          ) : null}
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <div>
       <Drawer open={open} onClose={() => setOpen(false)}>
@@ -21,13 +54,17 @@ const ImgList = ({ classes, state, db, states, setNav }) => {
         <MenuIcon />
       </IconButton>
       <Loading test={!!imgs}>
-        {(imgs || []).map(img => {
-          return (
-            <div className={classes.imgContainer}>
-              <img key={img.thumb} src={img.thumb} alt="" />
-            </div>
-          );
-        })}
+        <div className={classes.imgsContainer}>
+          {(imgs || []).map(img => {
+            console.log(img);
+            return (
+              <div key={img.id} className={classes.imgContainer}>
+                {renderIconsContainer(img)}
+                <img key={img.thumb} src={img.thumb} alt="" />
+              </div>
+            );
+          })}
+        </div>
       </Loading>
     </div>
   );
@@ -35,9 +72,30 @@ const ImgList = ({ classes, state, db, states, setNav }) => {
 
 export default withStyles(theme => {
   return {
+    iconsContainer: {
+      background: "#4f4f4f",
+      textAlign: "left"
+    },
+    icon: {
+      color: theme.palette.common.white,
+      margin: [[1, 5]]
+    },
+    drawingIconsContainer: {
+      background: theme.palette.primary.dark
+    },
+    drawingIcon: {
+      color: theme.palette.primary.dark.contrastText
+    },
+    imgsContainer: {
+      textAlign: "center"
+    },
     imgContainer: {
       display: "inline-block",
-      verticalAlign: "top"
+      margin: 2,
+      verticalAlign: "top",
+      "& img": {
+        maxWidth: "100%"
+      }
     },
     menuButton: {
       position: "fixed",
@@ -57,7 +115,6 @@ function useImages(db, state) {
   const [images, setImages] = useState(null);
 
   useEffect(() => {
-
     if (state === "__MARKED__") {
       getMarkedImages(db)
         .then(res => setImages(res))
