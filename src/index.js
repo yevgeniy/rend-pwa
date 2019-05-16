ef94aeeded986b1fded48bc27825f7c51d8fc9a0
Nimm <yandreyev@yahoo.com>
2019-05-15 21:58:16 -0500

    try install code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./install";
 
-const HttpsApp = ()=> {
-    return <App />;
-}
+const HttpsApp = () => {
+  return <App />;
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<HttpsApp />, rootElement);
