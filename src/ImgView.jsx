fc6a77a12e0dea3004a630babdf373c05142bf81
Nimm <yandreyev@yahoo.com>
2019-05-17 12:19:01 -0500

    asdf

--- src/ImgView.jsx
+++ src/ImgView.jsx
@@ -66,7 +66,8 @@ const ImgView = ({ classes, img, updateImage, setSelectedImage }) => {
     const loaded = () => {
       instance = panzoom(document.querySelector("#panthis"), {
         onTouch: function(e) {
-          return false;
+          for (let i in e) alert(i);
+          return true;
           // `e` - is current touch event.
           if (1.05 > zoom && zoom >= 1) return false; // tells the library to not preventDefault.
           return true;
