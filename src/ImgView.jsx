d63c22ed63ded077585076675e333901d6121724
Nimm <yandreyev@yahoo.com>
2019-05-17 11:49:22 -0500

    sadf

--- src/ImgView.jsx
+++ src/ImgView.jsx
@@ -52,10 +52,10 @@ const ImgView = ({ classes, img, updateImage, setSelectedImage }) => {
 
   useEffect(() => {
     const work = e => e.preventDefault();
-    document.body.addEventListener("touchmove", work);
+    document.addEventListener("touchmove", work);
     document.body.style.touchAction = "none";
     return () => {
-      document.body.removeEventListener("touchmove", work);
+      document.removeEventListener("touchmove", work);
       document.body.style.touchAction = "";
     };
   });
