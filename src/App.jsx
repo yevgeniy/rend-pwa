f036d7812ab86d7882940b97a9b5abafca553a74
gene <yandreyev@yahoo.com>
2019-10-04 09:48:58 -0500

    adsf

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 import Loading from "./Loading";
-import StatesView from "./StatesView";
-import { useStates, useDb } from './hooks';
-
+import { useStates, useDb } from "./hooks";
+const StatesView = React.lazy(() => import("./StatesView"));
 
 const useStyles = makeStyles(theme => {
-    return {
-        root: {
-        }
-    }
-})
+  return {
+    root: {}
+  };
+});
 
 const App = React.memo(() => {
-    const classes = useStyles()
-    const db = useDb();
-    let states = useStates(null);
-    const [nav, setNav] = useState(null);
-    if (!db)
-        return null;
-    return (
-        <div className={classes.root}>
-            <Loading test={!!states}>
-                {nav || <StatesView states={states} setNav={setNav} db={db} />}
-            </Loading>
-        </div>
-    );
+  const classes = useStyles();
+  const db = useDb();
+  let states = useStates(null);
+  const [nav, setNav] = useState(null);
+  if (!db) return null;
+  return (
+    <div className={classes.root}>
+      <Loading test={!!states}>
+        {nav || (
+          <Suspense fallback={<div>loading</div>}>
+            <StatesView states={states} setNav={setNav} db={db} />
+          </Suspense>
+        )}
+      </Loading>
+    </div>
+  );
 });
 
 export default App;
