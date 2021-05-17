948b189619ed63993a4d902d07e3642b6b3739d5
gene <yandreyev@yahoo.com>
2021-05-17 14:33:50 -0500

    feat: access

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { login, logout } from "./hooks";
@@ -6,10 +6,44 @@ import * as serviceWorker from "./serviceWorker";
 import "./install";
 
 const HttpsApp = () => {
+  const [a, seta] = useState(false);
+  const [b, setb] = useState(false);
+  const access = a && b;
   useEffect(() => {
     login();
     return () => logout();
   }, []);
+
+  if (!access) {
+    return (
+      <>
+        <button
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            height: "50px",
+            opacity: 0
+          }}
+          onClick={e => seta(true)}
+        >
+          let none but
+        </button>
+        <button
+          style={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            height: "50px",
+            opacity: 0
+          }}
+          onClick={e => setb(true)}
+        >
+          beauty enter
+        </button>
+      </>
+    );
+  }
 
   return <App />;
 };
